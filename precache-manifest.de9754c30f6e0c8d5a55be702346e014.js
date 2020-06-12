5558614187d1112dd3ca88402c5ddb4ac7da1016
yura6904 <45623333+yura6904@users.noreply.github.com>
2020-06-12 16:35:25 +0300

    Add files via upload

---
+++ precache-manifest.de9754c30f6e0c8d5a55be702346e014.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "790fe99455ab46afac32e64aa575f10b",
+    "url": "/biryulinyuri.github.io/index.html"
+  },
+  {
+    "revision": "2b572c1195472007bfa5",
+    "url": "/biryulinyuri.github.io/static/css/main.cbb2ece8.chunk.css"
+  },
+  {
+    "revision": "02ee9e0e2fcdd4d27109",
+    "url": "/biryulinyuri.github.io/static/js/2.e407f60c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/biryulinyuri.github.io/static/js/2.e407f60c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b572c1195472007bfa5",
+    "url": "/biryulinyuri.github.io/static/js/main.4412c25d.chunk.js"
+  },
+  {
+    "revision": "fc4c24116241fabcceca",
+    "url": "/biryulinyuri.github.io/static/js/runtime-main.f462157e.js"
+  },
+  {
+    "revision": "4b71f8137985eaa992d17a315997791e",
+    "url": "/biryulinyuri.github.io/static/media/user.4b71f813.png"
+  }
+]);
